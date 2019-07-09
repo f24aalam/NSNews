ce684048c1faf06698a482490c1b0b41750939f1
jasmeetsingh <jasmeetsinghkhokhar@gmail.com>
2019-07-09 17:20:50 +0530

    Everything Fragment Added

--- app/src/main/java/com/w3students/nsnews/MainActivity.java
+++ app/src/main/java/com/w3students/nsnews/MainActivity.java
@@ -60,12 +60,13 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
-
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_top_headlines);
 
         fragment = null;
         fragment = new TopHeadlines();
--- app/src/main/java/com/w3students/nsnews/fragment/Everything.java
+++ app/src/main/java/com/w3students/nsnews/fragment/Everything.java
@@ -33,7 +33,6 @@ import dmax.dialog.SpotsDialog;
 
 public class Everything extends Fragment {
 
-
     RecyclerView articleList;
     List<Article> articles;
     AlertDialog alertDialog;
@@ -42,6 +41,70 @@ public class Everything extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_everything,container,false);
+
+        articleList = view.findViewById(R.id.everything_view);
+
+        String url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=8c836e5a02774abc9ecaae10a1d1f5f6";
+        articles = new ArrayList<>();
+
+        alertDialog = new SpotsDialog
+                .Builder()
+                .setContext(view.getContext())
+                .setMessage("Loading Top Headlines...")
+                .setCancelable(false)
+                .build();
+
+        alertDialog.show();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if(response.getString("status").equalsIgnoreCase("ok")){
+                                JSONArray jsonArray = response.getJSONArray("articles");
+                                for(int i = 0; i < jsonArray.length(); i++){
+
+                                    Article article = new Article();
+                                    ArticleSource articleSource = new ArticleSource();
+
+                                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                    JSONObject jsonSourceObject = jsonObject.getJSONObject("source");
+
+                                    articleSource.setId(jsonSourceObject.getString("id"));
+                                    articleSource.setName(jsonSourceObject.getString("name"));
+
+                                    article.setArticleSource(articleSource);
+                                    article.setAuthor(jsonObject.getString("author"));
+                                    article.setTitle(jsonObject.getString("title"));
+                                    article.setDescription(jsonObject.getString("description"));
+                                    article.setUrl(jsonObject.getString("url"));
+                                    article.setUrlToImage(jsonObject.getString("urlToImage"));
+                                    article.setPublishedAt(jsonObject.getString("publishedAt"));
+                                    article.setContent(jsonObject.getString("content"));
+
+                                    articles.add(article);
+
+                                }
+
+                                ArticleAdapter adapter = new ArticleAdapter(articles, view.getContext() );
+                                articleList.setAdapter(adapter);
+                                articleList.setLayoutManager(new LinearLayoutManager(new AppCompatActivity()));
+
+                                alertDialog.dismiss();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        VolleyRequestQueue.getInstance(view.getContext()).addToRequestQueue(request);
         return view;
     }
 
@@ -49,5 +112,6 @@ public class Everything extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle(R.string.menu_everything);
+
     }
 }
