f2137f9b3145339f0f921df83948f5eeb528b683
Pushpak Goswami <pushpakgoswami645@gmail.com>
2019-07-11 09:40:02 -0700

    icons with one change in main activity

---
+++ app/src/main/java/com/w3students/nsnews/ArticleActivity.java
@@ -0,0 +1,29 @@
+package com.w3students.nsnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.w3students.nsnews.adapters.ArticleAdapter;
+import com.w3students.nsnews.models.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleActivity extends AppCompatActivity {
+    RecyclerView article_activity;
+    List<Article> articles;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+        article_activity=findViewById(R.id.source_category_flag);
+        articles=new ArrayList<>();
+        ArticleAdapter articleAdapter=new ArticleAdapter(articles,this);
+        article_activity.setAdapter(articleAdapter);
+        article_activity.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
