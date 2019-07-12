3423244cf1f55c5af78bee52477bc51654e11099
ankush <ankushaglawe123@gmail.com>
2019-07-12 19:10:57 +0530

    filter added

---
+++ app/src/main/java/com/w3students/nsnews/FilterActivity.java
@@ -0,0 +1,26 @@
+package com.w3students.nsnews;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class FilterActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Filter");
+
+
+    }
+
+}
